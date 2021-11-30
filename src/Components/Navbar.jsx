271ed207f7a43e8482b99bcac8be9cb614728fce
krishp058 <74632222+krishp058@users.noreply.github.com>
2021-11-30 22:44:09 +1100

    Made improvements

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -20,21 +20,21 @@ const Navbar = ({ currentPageShop }) => {
 
   return (
     <div className=''>
-      <div className='grid grid-cols-12 gap-4 pt-4 row-span-2 grid-rows-1 pb-3 bg-gray-800'>
-        <div className='col-start-4 col-span-1'>
+      <div className='grid grid-cols-12 gap-4 pt-4 row-span-2 grid-rows-1 pb-2 bg-gray-800'>
+        <div>
           <a href='/' className='flex-shrink-0 flex items-center'>
             <img
               src='https://img.icons8.com/color/48/000000/shooting-stars.png'
               alt='Shooting Stars'
-              className=' transform scale-75'
+              className=' transform scale-75 ml-4'
             />
-            <h1 className='text-gray-200 font-mono antialiased text-2xl hidden lg:block h-8 w-auto ml-3'>
-              U bay
+            <h1 className='text-gray-200 font-mono font-bold antialiased text-2xl hidden lg:block h-8 w-auto ml-3 mb-1'>
+              BKSA
             </h1>
           </a>
         </div>
         <div className='flex'>
-          <div className='mr-2'>
+          <div className='ml-8 mr-2'>
             <Link to='/shop'>
               <Button1 name='shop' />
             </Link>
@@ -59,7 +59,7 @@ const Navbar = ({ currentPageShop }) => {
             <button
               type='button'
               onClick={signInWithGoogle}
-              class='flex items-center justify-center w-full px-6 py-2.5 mx-2 text-sm font-medium text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-400 focus:bg-blue-400 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed' disabled
+              className='flex items-center justify-center w-full px-6 py-2.5 mr-2 ml-24 text-sm font-medium text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-400 focus:bg-blue-400 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed' disabled
             >
               <svg class='w-4 h-4 mx-2 fill-current' viewBox='0 0 24 24'>
                 <path d='M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z'></path>
@@ -73,7 +73,7 @@ const Navbar = ({ currentPageShop }) => {
             <button
               type='button'
               onClick={signInWithGoogle}
-              class='flex items-center justify-center w-full px-6 py-2.5 mx-2 text-sm font-medium text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-400 focus:bg-blue-400 focus:outline-none'
+              class='flex items-center justify-center w-full px-6 py-2.5 mr-2 ml-24 text-sm font-medium text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-400 focus:bg-blue-400 focus:outline-none'
             >
               <svg class='w-4 h-4 mx-2 fill-current' viewBox='0 0 24 24'>
                 <path d='M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z'></path>
